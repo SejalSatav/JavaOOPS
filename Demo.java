7b303187887bc700f99d5f2e43c726f32309a016
Kshiteej Jain <145803361+SejalSatav@users.noreply.github.com>
2024-04-05 02:25:54 -0400

    two dimensional array

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ public class Demo{
 
     public static void main(String a[])
     {
-        // int nums[] = new int[4];
+        // int nums[] = new int[4];  //single dimension array
         // nums[0] = 3;
         // nums[1] = 5;
         // nums[2] = 9;
@@ -13,21 +13,21 @@ public class Demo{
         // System.out.println(nums[i]);
         // }
 
-        int nums[][] = new int[3][4];
+        int nums[][] = new int[3][4]; //two dimensional array
 
-        for (int i=0;i<3;i++)
-        {
-            for (int j=0;j<4;j++)
-            {
-                nums[i][j] = (int)(Math.random() * 100);
-            }
-        }
+        // for (int i=0;i<3;i++)
+        // {
+        //     for (int j=0;j<4;j++)
+        //     {
+        //         nums[i][j] = (int)(Math.random() * 100);
+        //     }
+        // }
 
-        for (int i=0; i<3;i++)
+        for (int n[] : nums)   //advanced for loop for to dimensional array
         {
-            for (int j=0;j<4;j++)
+            for (int m : n)
             {
-                System.out.print(nums[i][j] + " ");
+                System.out.print(nums m + " ");
             }
 
             System.out.println();
