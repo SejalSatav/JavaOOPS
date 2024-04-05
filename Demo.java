b9cfbac696a5a8afdd982a8a156e7f8560205881
Kshiteej Jain <145803361+SejalSatav@users.noreply.github.com>
2024-04-05 01:57:20 -0400

    Array program

---
+++ Demo.java
@@ -0,0 +1,16 @@
+public class Demo{
+
+    public static void main(String a[])
+    {
+        int nums[] = new int[4];
+        nums[0] = 3;
+        nums[1] = 5;
+        nums[2] = 9;
+        nums[3] = 12;
+
+        for(int i=0;i<4;i++)
+        {
+        System.out.println(nums[i]);
+        }
+    }
+}
